d247f8965d8879a15c2bef9e9e47a490af4fbb35
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-05-28 16:01:48 -0500

    added CKEDitor for article writing

--- src/pages/form_minipages/page2.jsx
+++ src/pages/form_minipages/page2.jsx
@@ -1,8 +1,34 @@
+import React, {Component} from 'react';
 import NextButton from "../../components/form_components/next_btn";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 function PageTwo() {
+
+    const editorConfig = { //CONFIGURES EDITOR SETTINGS
+        // CKEditor configuration options
+        toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote'],
+        resize_maxWidth: '100%',
+        resize_maxHeight: '100%',
+      };
+
     return(
-        <div className="flex flex-col col-span-2 items-center">hihihi</div>
+        <div className="flex flex-col col-span-2 items-center h-[85vh] overflow-hidden">
+            <div className='w-11/12 h-[80%] m-5 overflow-hidden'>
+                <div className='overflow-y-scroll h-full w-full'>
+                    <CKEditor
+                        editor={ ClassicEditor }
+                        data="<p>This is your canvas. Write to your heart's content!</p>"
+                        config={editorConfig}
+                        onReady={(editor) => {
+                            editor.ui.view.editable.element.style.height = '100%';
+                        }}
+                    />
+                </div>
+            </div>
+            <NextButton />
+        </div>
     )
 }
 
